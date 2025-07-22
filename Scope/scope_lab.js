3b12deb2fb772d02dd01dbafdce867075263ebf7
cwonl <won.c.1@pg.com>
2025-07-22 17:30:09 -0400

    message

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -12,5 +12,16 @@ const blockConst = "I'm a block-scoped const";
 }
 // Global scope
 //Block Scope
-console.log(blockVar);
-console.log(blockLet);
+//console.log(blockVar);
+//console.log(blockLet);
+
+function show(){
+    var functionVar = "I'm a block-scoped var";
+    let functionLet = "I'm a block-scoped let";
+    const functionConst = "I'm a block-scoped const";
+    }
+    show();
+    
+    console.log(functionVar); // Throws ReferenceError
+    console.log(functionLet); // Throws ReferenceError
+    console.log(functionConst); // Throws ReferenceError
