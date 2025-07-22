9e020a771b5f310e0021d3e176696b0ec65ff469
cwonl <won.c.1@pg.com>
2025-07-22 17:28:13 -0400

    message

---
+++ Scope/scope_lab.js
@@ -0,0 +1,16 @@
+// Global scope
+var globalVar = "I'm a global variable";
+let globalLet = "I'm also global, but scoped with let";
+const globalConst = "I'm a global constant";
+
+
+{
+// Block scope
+var blockVar = "I'm a block-scoped var";
+let blockLet = "I'm a block-scoped let";
+const blockConst = "I'm a block-scoped const";
+}
+// Global scope
+//Block Scope
+console.log(blockVar);
+console.log(blockLet);
